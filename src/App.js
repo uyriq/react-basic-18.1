d339def3bf3e19b2cb27de00fda7fe73c4ce3ea5
uyriq <105006434+uyriq@users.noreply.github.com>
2022-05-17 23:35:56 +0300

    doubled

--- src/App.js
+++ src/App.js
@@ -2,12 +2,6 @@ import React from "react";
 import PropTypes from 'prop-types';
 import "./styles.css";
 
-export default class App extends React.Component {
-  render() {
-    return <div className="App"></div>;
-  }
-}
-
 const messagePropTypes = PropTypes.shape({
   id: PropTypes.number.isRequired,
   user: PropTypes.string.isRequired,
