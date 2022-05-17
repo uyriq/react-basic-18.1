5ef118d2178183373f1653d2069dcce5e28786f6
uyriq <105006434+uyriq@users.noreply.github.com>
2022-05-17 23:33:48 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,8 +1,82 @@
 import React from "react";
+import PropTypes from 'prop-types';
 import "./styles.css";
 
 export default class App extends React.Component {
   render() {
     return <div className="App"></div>;
+  }
+}
+
+const messagePropTypes = PropTypes.shape({
+  id: PropTypes.number.isRequired,
+  user: PropTypes.string.isRequired,
+  replyTo: PropTypes.number,
+  text: PropTypes.string.isRequired
+});
+
+const Message = ({ message, repliedMessage, className = 'message' }) => (
+  <div className={className}>
+    {repliedMessage && <RepliedMessage message={repliedMessage} />}
+    <h3>{message.user}</h3>
+    <p>{message.text}</p>
+  </div>
+);
+
+Message.propTypes = {
+  message: messagePropTypes.isRequired,
+  repliedMessage: messagePropTypes,
+  className: PropTypes.string
+};
+
+const RepliedMessage = ({ message }) => <Message message={message} className={'replied-message'} />;
+
+RepliedMessage.propTypes = {
+  /* {Ваш код здесь} */
+};
+
+const Chat = ({ thread }) => (
+  <div className="tread">
+    {thread.map(message => {
+      const repliedMessage = thread.find(m => m.id === message.replyTo);
+
+      return <Message key={message.id} repliedMessage={repliedMessage} message={message} />;
+    })}
+  </div>
+);
+
+Chat.propTypes = {
+  /* {Ваш код здесь} */
+};
+
+export default class App extends React.Component {
+  state = {
+    thread: [
+      {
+        id: 1,
+        user: 'Тамара',
+        text: 'Всем привет! Кто в курсе, когда в нашем доме отключат горячую воду?'
+      },
+      {
+        id: 2,
+        user: 'Алексей',
+        replyTo: 1,
+        text: 'В подъезде висит объявление, скоро буду там, сфотографирую и пришлю сюда'
+      },
+      {
+        id: 3,
+        user: 'Катя',
+        replyTo: 2,
+        text: 'О! Спасибо! Ждём! :)'
+      }
+    ]
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Chat thread={this.state.thread} />
+      </div>
+    );
   }
 }
